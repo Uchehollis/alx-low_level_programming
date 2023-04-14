5878515c2ec996576d930df25e106496f331600f
Uchehollis <uchehollis257@gmail.com>
2023-04-14 12:16:00 -0700

    a program that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,28 +1,43 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
-* _atoi - converts a string to an integer
-* @s: string to be converted
-* Return: the int converted from the string
-*/
+ * _is_digit - checks if a character is a digit
+ * @c: the character to check
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+/**
+ * _digit_to_int - converts a character digit to an integer
+ * @c: the character digit to convert
+ * Return: the integer value of the digit
+ */
+int _digit_to_int(char c)
+{
+	return (c - '0');
+}
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to convert
+ * Return: the integer value of the string
+ */
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
 
 	i = 0;
-
 	d = 0;
-
 	n = 0;
-
 	len = 0;
-
 	f = 0;
-
 	digit = 0;
 
 	while (s[len] != '\0')
-
 		len++;
 
 	while (i < len && f == 0)
@@ -30,20 +45,20 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			++d;
 
-		if (s[i] >= '0' && s[i] <= '9')
+		if (_is_digit(s[i]))
 		{
-			digit = s[i] - '0';
+			digit = _digit_to_int(s[i]);
 
 			if (d % 2)
-
 				digit = -digit;
 
 			n = n * 10 + digit;
 
 			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
 
+			if (!_is_digit(s[i + 1]))
 				break;
+
 			f = 0;
 		}
 		i++;
@@ -55,25 +70,23 @@ int _atoi(char *s)
 	return (n);
 }
 
-
 /**
-* main - multiplies two numbers
-* @argc: number of arguments
-* @argv: array of arguments
-* Return: 0 (Success), 1 (Error)
-*/
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (Success), 1 (Error)
+ */
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 
-	if (argc < 3 || argc > 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
 	num1 = _atoi(argv[1]);
-
 	num2 = _atoi(argv[2]);
 
 	result = num1 * num2;
