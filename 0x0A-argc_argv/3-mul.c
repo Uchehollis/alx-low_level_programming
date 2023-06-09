bc1f98f992e02064e4144d7c8b928899f94327e4
Uchehollis <uchehollis257@gmail.com>
2023-06-09 16:53:45 -0700

    C - argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,83 +1,63 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* _atoi - converts a string to an integer
-* @s: string to be converted
-* Return: the int converted from the string
-*/
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	int j, e, o, len, g, num;
 
-	i = 0;
-
-	d = 0;
-
-	n = 0;
-
+	j = 0;
+	e = 0;
+	o = 0;
 	len = 0;
-
-	f = 0;
-
-	digit = 0;
-
+	g = 0;
+	num = 0;
 	while (s[len] != '\0')
-
 		len++;
-
-	while (i < len && f == 0)
+	while (j < len && g == 0)
 	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-
-			if (d % 2)
-
-				digit = -digit;
-
-			n = n * 10 + digit;
-
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-
-				break;
-			f = 0;
-		}
-		i++;
+	if (s[j] == '-')
+		++e;
+	if (s[j] >= '0' && s[j] <= '9')
+	{
+		num = s[j] - '0';
+	if (e % 2)
+		num = -num;
+		o = o * 10 + num;
+		g = 1;
+	if (s[j + 1] < '0' || s[j + 1] > '9')
+	break;
+		g = 0;
 	}
-
-	if (f == 0)
+		j++;
+	}
+	if (g == 0)
 		return (0);
-
-	return (n);
+		return (o);
 }
-
-
 /**
-* main - multiplies two numbers
-* @argc: number of arguments
-* @argv: array of arguments
-* Return: 0 (Success), 1 (Error)
-*/
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
+	int result, dig1, dig2;
 
 	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	num1 = _atoi(argv[1]);
-
-	num2 = _atoi(argv[2]);
-
-	result = num1 * num2;
-
-	printf("%d\n", result);
-	return (0);
+	dig1 = _atoi(argv[1]);
+	dig2 = _atoi(argv[2]);
+	result = dig1 * dig2;
+		printf("%d\n", result);
+		return (0);
 }
